6f0d622333f7ebee7611bd5aacb0689c4c7c7d1b
atimchik <indatim@gmail.com>
2023-02-26 02:26:47 +0200

    goit-js-hw-07
    Зроблено Задача 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const picContainer = document.querySelector('.gallery');
+const picMaker = createPicMaker(galleryItems);
+picContainer.insertAdjacentHTML('beforeend', picMaker);
+
+function createPicMaker(items) {
+    return galleryItems.map(item => {
+        return `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" title="${item.description}"/>
+</a>`}).join('');
+}
+
+new SimpleLightbox('.gallery a', { captionDelay: 250, navText: ['←','→'] });
+
+// console.log(galleryItems)
